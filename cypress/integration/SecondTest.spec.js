4b915883893844f79732e165c3d1ecd89c560b9d
AlinaNice <alinamazur82@gmail.com>
2021-03-25 10:50:07 -0400

    baseline - git test2 - the rest of code

---
+++ cypress/integration/SecondTest.spec.js
@@ -0,0 +1,16 @@
+import { createYield } from "typescript"
+
+
+describe('Test log out', () => {
+
+    beforeEach('log in to the app', () => {
+        cy.loginToApplication()
+
+    })
+
+    it('verify use can log out successfully', () => {
+        cy.contains('Settings').click()
+        cy.contains('Or click here to logout').click()
+        cy.get('.navbar-nav').should('contain','Sign up')
+    })
+})
